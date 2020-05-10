48c12440f70d004701ee50c9b815f0994345c0dd
Ana Sanchotene <anasanchotenes@gmail.com>
2020-05-10 12:02:10 -0300

    added menu_buttons

---
+++ cypress/page_objects/menus_buttons.po.js
@@ -0,0 +1,26 @@
+class menu_buttons{
+  constructor(){
+      //dropdown menus
+
+      //checkboxes
+      this.checkOption1 = '[value="option-1"]';
+      this.checkOption2 = '[value="option-2"]';
+      this.checkOption3 = '[value="option-3"]';
+      this.checkOption4 = '[value="option-4"]';
+
+      //radio buttons
+      this.radioGreen = '[value="green"]'; 
+      this.radioBlue = '[value="blue"]';
+      this.radioYellow = '[value="yellow"]';
+      this.radioOrange = '[value="orange"]';
+      this.radioPurple = '[value="purple"]';
+
+      //selected and disable
+      this.radioLettuce = '[value="lettuce"]';
+      this.radioCabbage = '[value="cabbage"]';
+      this.radioPumpkin = '[value="pumpkin"]';
+      this.dropdownFruits = '[id="fruit-selects"]';
+  }
+
+} export default new menu_buttons();
+
